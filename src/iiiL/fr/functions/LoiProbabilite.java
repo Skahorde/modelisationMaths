19e3e2a993fe16b6166706789448620af5a01fd7
Emmanuel Perez <contact@emmanuel-perez.com>
2018-04-19 21:22:59 +0200

    second commit

---
+++ src/iiiL/fr/functions/LoiProbabilite.java
@@ -0,0 +1,14 @@
+/*
+ * LoiProbabilite.java                                   19 avr. 2018
+ * 3IL2 2017-2018 groupe B, ni copyright ni copyleft
+ */
+package iiiL.fr.functions;
+
+/**
+ * TODO commenter les responsabilités de la classe
+ * @author conta
+ *
+ */
+public class LoiProbabilite {
+
+}
---
+++ src/iiiL/fr/ihm/Interface.java
@@ -0,0 +1,25 @@
+/*
+ * Interface.java                                   19 avr. 2018
+ * 3IL2 2017-2018 groupe B, ni copyright ni copyleft
+ */
+package iiiL.fr.ihm;
+
+import iiiL.fr.functions;
+
+/**
+ * TODO commenter les responsabilités de la classe
+ * @author conta
+ *
+ */
+public class Interface {
+
+    /**
+     * TODO commenter le rôle de cette méthode
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
