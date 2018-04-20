9e5144d228f453272dd8fbab751673ea6938b4db
Emmanuel Perez <contact@emmanuel-perez.com>
2018-04-20 22:22:12 +0200

    Ajout loi Normale

--- src/iiiL/fr/functions/LoiProbabilite.java
+++ src/iiiL/fr/functions/LoiProbabilite.java
@@ -4,11 +4,34 @@
  */
 package iiiL.fr.functions;
 
+import java.lang.Math;
+
 /**
  * TODO commenter les responsabilités de la classe
  * @author conta
  *
  */
 public class LoiProbabilite {
-
+    
+        /**
+         * Loi normale - Loi de probabilité f(x) = 1 / (sigma * racine(2 * pi)) * exp((x - m²) / (2 * sigma²)) 
+         * @param m reel représentant la moyenne de la fonction
+         * @param sigma reel positif représentant l'écart-type de la fonction
+         * @param x variable aléatoire
+         * @return le résultat de la loi normale après tirage
+         */
+        public static double loiNormale(double m, double sigma, int x) {
+                /** Le résultat final */
+                double result = 0.0;
+                
+                if (sigma < 0) {
+                        System.out.println("Impossible ! Le sigma est négatif !");
+                        return result = -1;
+                } else if (x < 0) {
+                        System.out.println("Impossible ! Le x est négatif !");
+                        return result = -1;
+                } else {
+                        return result = 1 / (sigma * Math.sqrt(2 * Math.PI)) * Math.exp((x - Math.pow(m, 2)) / (2 * Math.pow(sigma, 2)));
+                }            
+        }
 }
