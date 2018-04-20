eadff1be4972738a46354d17c7524b818c73bcad
Emmanuel Perez <contact@emmanuel-perez.com>
2018-04-20 22:28:46 +0200

    Ajout loi de Weibull

--- src/iiiL/fr/functions/LoiProbabilite.java
+++ src/iiiL/fr/functions/LoiProbabilite.java
@@ -4,11 +4,37 @@
  */
 package iiiL.fr.functions;
 
+import java.lang.Math;
+
 /**
  * TODO commenter les responsabilités de la classe
  * @author conta
  *
  */
 public class LoiProbabilite {
-
+    
+        /**
+         * Loi de Weibull - Loi de probabilité f(x) = (k / lambda) * (x / lambda)^(k-1) * exp(-x / lambda)^k 
+         * @param k reel positif représentant le paramètre de forme
+         * @param lambda reel positif représentant le paramètre d'échelle de la distribution
+         * @param x variable aléatoire
+         * @return le résultat de la loi de Weibull après tirage
+         */
+        public static double loiDeWeibull(double k, double lambda, int x) {
+                /** Le résultat final */
+                double result = 0.0;
+                
+                if (k < 0) {
+                        System.out.println("Impossible ! Le sigma est négatif !");
+                        return result = -1;
+                } else if (lambda < 0) {
+                        System.out.println("Impossible ! Le lambda est négatif !");
+                        return result = -1;
+                } else if (x < 0) {
+                        System.out.println("Impossible ! Le x est négatif !");
+                        return result = -1;
+                } else {
+                        return result = (k / lambda) * Math.pow((x / lambda), (k-1)) * Math.exp(Math.pow((-x / lambda), k));
+                }            
+        }
 }
